91344348d1e663416be38c2d6b8b026320e4a0ed
Yoshua-chan <31251884+Yoshua-chan@users.noreply.github.com>
2023-09-20 01:55:46 +0200

    added basic MPU functions and definitions for MPU registers, it compiles, not tested yet

--- firmware/fc-stm32/Core/Inc/mpu6050.h
+++ firmware/fc-stm32/Core/Inc/mpu6050.h
@@ -10,9 +10,76 @@ typedef struct
     I2C_HandleTypeDef *hi2c;
 } MPU6050_STRUCT;
 
+#define AD0 0
+#define MPU6050_ADDR	0b1101000 | (AD0)
 
-#define MPU6050_ADDR 0x68
+#define PWR_MGMT_1_REG  0x6B
+#define SMPLRT_DIV_REG  0x19
+#define CONFIG_REG      0x1A
+#define INT_ENABLE_REG  0x38
+
+// ======= ACCELEROMETER ======= //
+// Accelerometer range
+#define AFS_SEL 1
+
+/**
+  * @brief sensitivity scale factor in LSB per g
+*/
+#define ACC_LSB_PER_G (16384.0f / pow(2, AFS_SEL))
+#define ACC_SCALE_FACTOR (pow(2, AFS_SEL) / 16384.0)
+
+/**
+  * @brief
+  * Address of accelerometer config register.
+  * Bit 7: XA_ST
+  * Bit 6: YA_ST
+  * Bit 5: ZA_ST
+  * Bits 4-3: AFS_SEL[1:0]
+*/
+#define ACC_CONFIG_REG 0x1C
+
+/**
+  * @brief
+  * Address of the first register with accelerometer data.
+  * The values are layed out in the order of X, Y, Z, with
+  * 16 bits per axix in Big Endian (MSB first).
+*/
+#define ACC_REG_START 0x3B
+
+// ======= GYROSCOPE ========= //
+// Gyroscope range
+#define FS_SEL 1
+#define GYRO_SCALE_FACTOR (pow(2, FS_SEL) / 131.0)
+
+/**
+  * @brief
+  * Address of the first register with gyroscope data.
+  * The values are layed out in the order of X, Y, Z, with
+  * 16 bits per axix in Big Endian (MSB first).
+*/
+#define GYRO_REG_START 0x43
+
+/**
+  * @brief
+  * Address of gyroscope config register.
+  * Bit 7: XG_ST
+  * Bit 6: YG_ST
+  * Bit 5: ZG_ST
+  * Bits 4-3: FS_SEL[1:0]
+*/
+#define GYRO_CONFIG_REG 0x1B
+
+#define INT_PIN_CFG_REG 0x37
+#define I2C_BYPASS_EN 1
+
+#define USER_CTRL_REG 0x6A
+#define I2C_MST_EN 5
+
 
 HAL_StatusTypeDef mpu6050_read_byte(MPU6050_STRUCT *mpu, uint8_t addr, uint8_t *data);
+HAL_StatusTypeDef MPU_init(MPU6050_STRUCT *mpu);
+HAL_StatusTypeDef MPU_read_acc(MPU6050_STRUCT *mpu, int16_t output[]);
+HAL_StatusTypeDef MPU_read_gyro(MPU6050_STRUCT *mpu, int16_t output[]);
+
 
 #endif // mpu6050
--- firmware/fc-stm32/Core/Src/mpu6050.c
+++ firmware/fc-stm32/Core/Src/mpu6050.c
@@ -1,6 +1,116 @@
 #include "mpu6050.h"
 
+#define HAL_error_check(status) if(status != HAL_OK) { return status; }
+
 HAL_StatusTypeDef mpu6050_read_byte(MPU6050_STRUCT *mpu, uint8_t addr, uint8_t *data)
 {
     return HAL_I2C_Mem_Read(mpu->hi2c, MPU6050_ADDR<<1, addr, 1, data, 1, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef MPU_init(MPU6050_STRUCT *mpu) {
+    // TODO: make this prettier
+    // TODO: make a structure for MPU6050 config for easier configuration
+    uint8_t i2c_buffer[2];
+
+    int status = HAL_OK;
+
+    *i2c_buffer = 7; // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            SMPLRT_DIV_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = 1; // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            PWR_MGMT_1_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = 0; // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            CONFIG_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = 24; // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            GYRO_CONFIG_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = AFS_SEL << 3;
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            ACC_CONFIG_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = FS_SEL << 3;
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            GYRO_CONFIG_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = 1; // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            INT_ENABLE_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    // Enable I2C pass-through mode
+    *i2c_buffer = (1 << I2C_BYPASS_EN); // FIXME: Magic value
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            INT_PIN_CFG_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    *i2c_buffer = 0;
+    status = HAL_I2C_Mem_Write(mpu->hi2c, MPU6050_ADDR << 1, 
+                            USER_CTRL_REG, 1,
+                            i2c_buffer, 1,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPU_read_acc(MPU6050_STRUCT *mpu, int16_t output[]) {
+    HAL_StatusTypeDef status;
+    uint8_t i2c_buffer[6];
+
+    status = HAL_I2C_Mem_Read(mpu->hi2c, MPU6050_ADDR << 1,
+                            ACC_REG_START, 1,
+                            i2c_buffer, 6,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    for(uint8_t i = 0; i < 3; i++) {
+        output[i] = (i2c_buffer[2 * i] << 8) | i2c_buffer[2 * i + 1];
+    }
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef MPU_read_gyro(MPU6050_STRUCT *mpu, int16_t output[]) {
+    HAL_StatusTypeDef status;
+    uint8_t i2c_buffer[6];
+
+    status = HAL_I2C_Mem_Read(mpu->hi2c, MPU6050_ADDR << 1,
+                            GYRO_REG_START, 1,
+                            i2c_buffer, 6,
+                            HAL_MAX_DELAY);
+    HAL_error_check(status);
+
+    for(uint8_t i = 0; i < 3; i++) {
+        output[i] = (i2c_buffer[2 * i] << 8) | i2c_buffer[2 * i + 1];
+    }
+
+    return HAL_OK;
 }
