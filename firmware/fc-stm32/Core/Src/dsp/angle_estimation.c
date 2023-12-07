019354c1bd356fd2104661d7b66eef08e945cafa
kflpk <31251884+kflpk@users.noreply.github.com>
2023-12-07 02:13:39 +0100

    added function for updating euler angles base on gyro readings

--- firmware/fc-stm32/Core/Src/dsp/angle_estimation.c
+++ firmware/fc-stm32/Core/Src/dsp/angle_estimation.c
@@ -27,3 +27,28 @@ void Get_Roll_Pitch(float acc_buf[3], float angles[2]){
 
   angles[1] = asin(acc_buf[0]);
 }
+/**
+ * @brief 
+ * 
+ * @param angles current estimation of euler angles [roll, pitch, yaw]
+ * @param gyro gyro inputs [x, y, z]
+ * @param dt time since last gyro sample
+ */
+
+void update_euler_angles(float angles[3], float gyro[3], float dt) {
+  const int roll = 0, pitch = 1, yaw = 2;
+  const int x    = 0, y     = 1, z   = 2;
+
+  float sin_psi   = sin(angles[roll]);
+  float cos_psi   = cos(angles[roll]), cos_theta = cos(angles[pitch]);
+  float tan_theta = tan(angles[pitch]);
+
+  float angle_change[3] = {gyro[x] + sin_psi * cos_theta * gyro[y],
+                           cos_psi * gyro[y] - sin_psi * gyro[z],
+                           (sin_psi / cos_theta) * gyro[y] + (cos_psi / cos_theta) * gyro[z]};
+
+  for(int i = 0; i < 3; i++) {
+    // NOTE: this function currently returns the new euler angles but may be chaged to return the deltas for use in a complementary filter
+    angles[i] += angle_change[i] * dt;   
+  }
+}
